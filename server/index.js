401b790187662c683bc2b2097ada97b34db912ef
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-03-09 19:29:59 -0800

    Updated the front-end URL

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "https://workout-tracking-rho.vercel.app",
+  origin: "https://workout-record.vercel.app",
 };
 
 const app = express();

778f4fefa026675246fa385498ecb4670563c098
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-03-09 18:28:58 -0800

    Removed the get from the index.js

--- server/index.js
+++ server/index.js
@@ -16,9 +16,9 @@ app.use(cors(corsOptions));
 app.use("/api/user", userRoute);
 app.use("/api/log", logRoute);
 
-app.get("/", (req, res) => {
-  res.json("Hello server!");
-});
+// app.get("/", (req, res) => {
+//   res.json("Hello server!");
+// });
 
 app.listen(5000, () => {
   console.log("Server is running");
